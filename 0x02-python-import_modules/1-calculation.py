15db79c8b513aafc2397c6cf00eed5d4e0cb7d0d
Ibukun16 <ib.agunbiade18@gmail.com>
2024-03-15 23:04:18 -0700

    My python code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """"Print the sum, difference, multiplication and division of given integers"""
+    """"Print the sum, difference, multiple and division of integers."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
