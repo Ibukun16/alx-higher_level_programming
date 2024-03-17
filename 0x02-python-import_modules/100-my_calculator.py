71b9ba0cc1f25c7a53ba7141e2b7618ee6cc7e1b
Ibukun16 <ib.agunbiade18@gmail.com>
2024-03-16 23:48:35 -0700

    perform basic math operations with imported functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,10 +10,11 @@ if __name__ == "__main__":
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/":div}
-    if sys.argv[2] not in list (ops.keys()):
+    op = sys.argv[2]
+    if op not in list (ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a,b)))
+    print("{} {} {} = {}".format(a, op, b, ops[op](a,b)))
