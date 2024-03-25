70984ee5a1221a7b448a24cecd53d1b57a131dde
Ibukun16 <ib.agunbiade18@gmail.com>
2024-03-25 10:05:16 -0700

    Multiply each value of a dictionary by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """A function that return a new dictionary with
+    each value multiplied by 2"""
+    return({key: a_dictionary[key] * 2 for key in a_dictionary})
