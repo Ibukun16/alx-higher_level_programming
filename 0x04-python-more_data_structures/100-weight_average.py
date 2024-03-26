49511974a43361cdfa62aa3db4667191b67e517c
Ibukun16 <ib.agunbiade18@gmail.com>
2024-03-26 05:05:16 -0700

    Find average weight of a tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """A function that returns the weighted average 
+    of all integers in a tuple."""
+    if not my_list:
+        return 0
+    tot_score = 0
+    tot_freq = 0
+    for col in my_list:
+        tot_score += (col[0] * col[1])
+        tot_freq += col[1]
+    return tot_score / tot_freq
