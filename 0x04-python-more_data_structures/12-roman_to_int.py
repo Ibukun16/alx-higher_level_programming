8cf090f00a45fce8e124a1829b89d5291b48ba5a
Ibukun16 <ib.agunbiade18@gmail.com>
2024-03-26 01:24:17 -0700

    convert roman numeral to arabic integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """"A function that converts roman figure
+    to integer."""
+    if not roman_string or type(roman_string) != str:
+        return 0
+
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for value in reversed(roman_string):
+        dgt = roman[value]
+        result += dgt if result < dgt * 5 else -dgt
+    return result
