f6412bc276a6aac4677d049a9164a96a2cc6015a
Ibukun16 <ib.agunbiade18@gmail.com>
2024-04-18 20:36:49 -0700

    Practise tasks on python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,9 @@ A module that defines a class that inherit from List
 
 class MyList(list):
     """"Inherit sorted printing from the built-in list class"""
+    def __init__(self):
+        """Initializing the object"""
+        super().__init__()
 
     def print_sorted(self):
         """Print list sorted in ascending order"""
