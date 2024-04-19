cdb6b30cbab483630783356321fcc0364baf02e0
Ibukun16 <ib.agunbiade18@gmail.com>
2024-04-18 20:06:21 -0700

    Practise Tasks of Python Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
+"""Practise Task 1 of Python Inheritance
+
 A module that defines a class that inherit from List
 """
 
 
 class MyList(list):
-    """"
-    Inherit sorted printing from the built-in list class
-    """
+    """"Inherit sorted printing from the built-in list class"""
+
     def print_sorted(self):
         """Print list sorted in ascending order"""
         print(sorted(self))
