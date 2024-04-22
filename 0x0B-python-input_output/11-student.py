d1b03c20f13afb80dae8ff4d6775c3e4a6aeb8cc
Ibukun16 <ib.agunbiade18@gmail.com>
2024-04-21 21:02:37 -0700

    Practise tasks on python input/output project

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -9,22 +9,24 @@ class Student:
     """A representation that defines a student class"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialization of the student class definition"""
+        """Initializing the student class definition"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a student instance
+        """Retrieves a dictionary rep of a student instance
 
-        If attrs is a list strings, only attributes name contain in the
-        list must be retrieved, else retrieve all attributes
+        If attrs is a list strings, only attributes name contain
+        in the list must be retrieved, else retrieve all attributes
 
         Arguments:
             attrs (list): (Optional) The attributes to be represented
         """
-        if (type(attrs) is list and all(type(mem) == str for mem in attrs)):
-            return {mem: getattr(self, mem) for mem in attrs if hasattr(self, mem)}
+        if (type(attrs) is list and
+                all(type(mem) == str for mem in attrs)):
+            return {mem: getattr(self, mem)
+                    for mem in attrs if hasattr(self, mem)}
         return self.__dict__
 
     def reload_from_json(self, json):
