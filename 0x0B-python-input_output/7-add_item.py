f4e1311e9dc6e364e880691e85bfab140d561a51
Ibukun16 <ib.agunbiade18@gmail.com>
2024-04-22 13:38:29 -0700

    Practise tasks on ALX input/output project

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Task 7 of ALX python input/output project
+
+A script that adds all arguments to a python list, and save
+them to a file.
+"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    newfile = load_from_json_file("add_item.json")
+except ValueError:
+    newfile = []
+
+for i in sys.argv[1:]:
+    newfile.append(i)
+
+save_to_json_file(newfile, "add_item.json")
