e89f32d3c0a64d76f6abe50f0eb9e60a8830a3ff
Ibukun16 <ib.agunbiade18@gmail.com>
2024-05-08 07:31:53 -0700

    Advance tasks 2 and 3 on python everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a 'LockedClass'."""
+
+class LockedClass:
+    """
+    This LockedClass prevent user from creating a new
+    instance attributes except if the the new instance
+    attribute is called 'first_name'.
+    """
+    __slots__ = ["first_name"]
