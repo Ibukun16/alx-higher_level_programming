7b14aa6f3d273698dab881b6772260ba3af8d125
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-18 02:59:21 -0700

    task 3 to 6 on SQL more queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- A script that creates database `hbtn_0d_usa` and table `states` in the database on MySQL server.
 -- If database `hbtn_0d_usa` or table `states` already exists, script should hold.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
