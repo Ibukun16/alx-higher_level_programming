2b7cfc3bb6ec7ca4b83f14824bbfd0ad6943b653
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-19 11:14:23 -0700

    Task 15 and 16 on SQL more queries project

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,4 +4,4 @@
 -- Each record displays: tv_shows.title, tv_genres.name
 -- Result of the display sorted in ascending order by the show title and genre name.
 -- SELECT statement usable only once.
-SELECT title, name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genres_id = tv_genres.id ORDER BY title ASC, name ASC;
+SELECT title, name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY title ASC, name ASC;
