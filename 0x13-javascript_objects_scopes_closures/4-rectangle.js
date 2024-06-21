f50eaa654fb5b5c0e5563f3b00c8f0c96e23777c
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-21 03:53:43 -0700

    Task 4 of javascript object

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let c = 0; c < this.height; c++) {
+      let p = '';
+      for (let n = 0; n < this.width; n++) {
+        p += 'X';
+      }
+      console.log(p);
+    }
+  }
+
+  rotate () {
+    const swap = this.width;
+    this.width = this.height;
+    this.height = swap;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
