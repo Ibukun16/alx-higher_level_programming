e009df8c2be3144b41d6d51f1ff6b825c4ee3283
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-20 20:49:17 -0700

    Task 7 of javascript warm up project

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-	console.log("Missing size");
+  console.log('Missing size');
 } else {
-	const n = Number(process.argv[2]);
-	for (let c = 0; c < n; c++) {
-		console.log("X".repeat(n));
-	}
+  const n = Number(process.argv[2]);
+  for (let c = 0; c < n; c++) {
+    console.log('X'.repeat(n));
+  }
 }
