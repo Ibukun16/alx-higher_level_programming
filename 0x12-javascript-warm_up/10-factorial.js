127cba6888243d68b6bd24135751851189ce5662
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-20 21:48:31 -0700

    Task 10 to 13 of javascript warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function factorial (n) {
-	if (n < 0) {
-		return (-1);
-	}
-	if (n === 0 || n === 1) {
-		return (1);
-	}
-	return (n * factorial(n - 1));
+function factorial (num) {
+  if (num < 0) {
+    return (-1);
+  }
+  if (num === 0 || isNaN(num)) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
 }
 console.log(factorial(Number(process.argv[2])));
