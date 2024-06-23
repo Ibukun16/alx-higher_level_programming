92e8961b1dabd0bfc7ad0ceb359ad6e336ddfb54
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-23 15:03:51 -0700

    Task 1 to 3 of Python-ORM project

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+"""
+A script that takes in arguments and displays all the values in the states
+table of hbtn_0e_0_usa where name matches the argument but safe from MySQL
+injections.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ = "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cusor()
+    item = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name = %s
+                ORDER by id ASC", (item,))
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
+    cur.close()
+    db.close()
