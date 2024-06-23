af54933dcaf7b9b37e46a1a7a1abfe99a026e085
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-23 15:25:48 -0700

    Task 1 to 3 of Python-ORM project

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cusor()
     item = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name = %s
-                ORDER by id ASC", (item,))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s
+                ORDER by states.id ASC", (item, ))
     rows = cur.fetchall()
     for r in rows:
         print(r)
