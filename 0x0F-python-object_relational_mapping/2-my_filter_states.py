f4f8ed583067998c8793ecaca2ecbc496447c330
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-23 15:36:26 -0700

    Task 1 to 3 of Python-ORM project

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM  states WHERE name
-                LIKE BINARY '{}'ORDER BY states.id ASC".format(sys.argv[4]))
+                LIKE BINARY '{}' ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for r in rows:
         print(r)
