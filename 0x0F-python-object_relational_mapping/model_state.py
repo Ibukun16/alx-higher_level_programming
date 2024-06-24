22520e2cff253250382be1648c4f58cec1f67fba
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-23 17:05:40 -0700

    Task 6 of python-ORM project

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A python file that contains class definition of a State and an instance
+Base as an instance of declarative_base().
+"""
+from sqalchemy import Column, Integer, String, MetaData
+from sqalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """A class with id and name attributes of each state"""
+
+    __tablename__ = 'states'
+    id = Colum(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
