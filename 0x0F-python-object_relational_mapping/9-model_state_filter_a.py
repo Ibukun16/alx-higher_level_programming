57f9f1c131fa3888395022cd1a5389fdaaf953c0
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-24 07:40:08 -0700

    Task 10 and 11 of Python-ORM project

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,3 +13,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sesionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter(State.name.like('%a%')):
+        print(f"{instance.id}: {instance.name}")
+    session.close()
