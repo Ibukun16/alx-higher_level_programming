63fae09293202488a742e289d483bd391fdbc786
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-24 08:49:50 -0700

    Task 11 of Python-ORM

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     instance = State(name="Louisiana")
     session.add(instance)
     new_instance = session.query(State).filter_by(name='Louisiana').first()
-    print(new_instance.id)
+    print(instance.id)
     session.commit()
     session.close()
