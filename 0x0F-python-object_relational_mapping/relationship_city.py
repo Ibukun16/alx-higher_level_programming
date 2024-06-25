36dc6c55127ce3042fe4f9191de16a36f388cbce
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-25 09:21:34 -0700

    Advanced task 1 of Python-ORM project

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,7 +11,7 @@ class City(Base):
     """
     A class that defines the attributes of each city
     """
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
