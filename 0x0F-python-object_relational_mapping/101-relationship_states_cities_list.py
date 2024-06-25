f4731939e502a778914556d7aec31d111e2e8839
Ibukun16 <ib.agunbiade18@gmail.com>
2024-06-25 15:21:27 -0700

    Advance task 101 of Python-ORM project

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id).all():
+    for instance in session.query(State).order_by(State.id):
         print(f"{instance.id}: {instance.name}")
         for inst_city in instance.cities:
             print("     ", end="")
