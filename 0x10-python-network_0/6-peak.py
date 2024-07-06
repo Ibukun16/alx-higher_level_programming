5ef4cee0bc827d97a831d97dd51c250bf61d166d
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-06 09:05:47 -0700

    update task 6 in python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,18 +7,17 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == []:
         return None
-    span = len(list_of_integers)
-    zero = 0
-    midpnt = ((span - zero) // 2) + zero
-    c = int(midpnt)
-    if span == 1:
-        return lists_of_integers[0]
-    if span == 2:
+    bgn = 0
+    size = len(list_of_integers)
+    c = ((size - bgn) // 2) + bgn
+    c = int(c)
+    if size == 1:
+        return list_of_integers[0]
+    if size == 2:
         return max(list_of_integers)
-    if list_of_integers[c] >= list_of_integers[c - 1] and\
-            list_of_integers[c] >= list_of_integers[c + 1]:
-        return list_of_integers[c]
-    if c > 0 and list_of_integers[c] < list_of_integers[c + 1]:
-        return find_peak(list_of_integers[c:])
-    if c > 0 and list_of_integers[c] < list_of_integers[c - 1]:
-        return find_peak(list_of_integers[:c])
+    if list_of_integers[size - 1] >= list_of_integers[size - 2]:
+        return list_of_integers[size - 1]
+    for i in range(0, size - 1):
+        if list_of_integers[i] >= list_of_integers[i - 1] and\
+                list_of_integers[i] >= list_of_integers[i + 1]:
+            return list_of_integers[i]
