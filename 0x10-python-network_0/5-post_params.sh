6dd4b2817baedbe133e14f8260fee015f13ab1a3
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-06 05:16:07 -0700

    Update python network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that displays the body of the response
-curl -s -X POST -d "test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
