2d20cee4fa28c9e8f8029ceb4dbab108cdf56cce
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-09 06:36:40 -0700

    Python network project 2 updated

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,8 +10,9 @@ import requests
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
+    url = "http://0.0.0.0:5000/search_user"
 
-    quest = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    quest = requests.post(url, data=payload)
     try:
         response = quest.json()
         if response == {}:
