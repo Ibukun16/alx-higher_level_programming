e47ed9ef740503bace4c06cee5101c57be8d11c2
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-09 04:48:44 -0700

    Python network project 2 tasks 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,9 @@
 """A python script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
+
 if __name__ == "__main__":
     quest = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(quest.text)))
-    print("\t-content: {}".format(quest.text))
+    print("\t- content: {}".format(quest.text))
