5876e2973b306074c63492626785c42380d53c45
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-30 21:05:23 -0700

    Node js project task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((n, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? ++n
+	: n;
+    }, 0));
+  }
+});
