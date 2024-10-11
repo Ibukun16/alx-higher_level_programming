f059c6f098c0906376ba78e4d2daeb25de64a6ab
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-11 10:47:25 -0700

    Update tasks accordingly

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for n in range(99):
-    print(f"{n} = 0x{n:x}")
+"""Print number in decimal and hexadecimal"""
+
+
+for n in range(0, 99):
+    print("{} = {}".format(n, hex(n)))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for n in range(100):
-    print(f"{n:02}", end=", " if n < 99 else "\n")
+"""Print number from 0 to 99"""
+
+
+for n in range(0, 100):
+    print("{}".format(n:02), end=", " if n < 99 else "\n")
