94c0d5c0d898089c708869aa3ebe689c18f9d8fb
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-11 10:38:36 -0700

    Update tasks

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 """Print alphabet in lower case"""
+
+
 for alphabet in range(97, 123):
-    print(f"{chr(alphabet)}", end="")
+    print("{}".format(chr(alphabet)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Print alphabet excluding letter e and q"""
+
+
 for alphabet in range(97, 123):
     if chr(alphabet) != 'q' and chr(alphabet) != 'e':
-        print(f"{chr(alphabet)}", end="")
+        print("{}".format(chr(alphabet)), end="")
